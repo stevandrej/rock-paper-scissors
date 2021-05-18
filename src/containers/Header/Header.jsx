d4f864e47bde707570fb445b0330aa1521666715
Andrej Stevkovski <stevandrej@yahoo.com>
2021-05-18 14:18:52 +0200

    Update Header.jsx
    New logo added

--- src/containers/Header/Header.jsx
+++ src/containers/Header/Header.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import Counter from '../../components/Counter/Counter';
 import './header.scss';
+import logo from '../../images/logo-bonus.svg'
 
 const Header = () => {
+
+	const gameMode = useSelector(state => state.game.gameMode);
+
 	return (
 		<header className='header'>
-			<h1 className='header__title'>ROCK<br />PAPER<br />SCISSORS</h1>
+			{gameMode === 'normal' ? <h1 className='header__title'>ROCK<br />PAPER<br />SCISSORS</h1> : <img src={logo} alt='logo' />}
 			<Counter />
 		</header>
 	)
